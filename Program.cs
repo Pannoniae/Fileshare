a601f3383f3596e885e0587f7f49cf5e1bfb0138
Pannoniae <prisznyak.zsombor@gmail.com>
2023-04-09 23:22:45 +0100

    - update working fileserver

--- Program.cs
+++ Program.cs
@@ -1,27 +1,46 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Fileshare.Data;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDirectoryBrowser();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.WebHost.UseStaticWebAssets();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-}
+app.UseDeveloperExceptionPage();
 
+var fileProvider = new PhysicalFileProvider(@"G:\music");
+var requestPath = "/share";
 app.UseStaticFiles();
+// Enable displaying browser links.
+app.UseStaticFiles(new StaticFileOptions {
+    FileProvider = fileProvider,
+    RequestPath = requestPath
+});
 
-app.UseRouting();
-
+app.UseFileServer(new FileServerOptions {
+    StaticFileOptions = {
+        ServeUnknownFileTypes = true,
+        HttpsCompression = HttpsCompressionMode.DoNotCompress
+    },
+    DirectoryBrowserOptions = {
+        FileProvider = fileProvider,
+        RequestPath = requestPath
+    },
+    FileProvider = fileProvider,
+    RequestPath = requestPath,
+    EnableDirectoryBrowsing = true
+});
+app.MapControllers();
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+//app.MapFallbackToPage("/_Host");
 
 app.Run();
